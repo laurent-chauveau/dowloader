9a35e92d5730d4262d0ccabdbdd148d5013cfdb3
Death <deathparadis@hotmail.fr>
2022-02-13 11:56:17 +0100

    Ajoutez des fichiers projet.

---
+++ videoDownload/videoDownload/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+
+namespace videoDownload
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Downloader();
+        }
+
+        private static void Downloader()
+        {
+            Console.WriteLine("URL de la vidéo");
+
+            string url = Console.ReadLine();
+
+            Console.WriteLine("Nom de la vidéo");
+            string videoName = Console.ReadLine();
+            string path = @"d:\tuto-grim-dark\" + videoName + ".mp4";
+            if (!string.IsNullOrWhiteSpace(url))
+            {
+                var client = new WebClient();
+                Uri uri = new Uri(url);
+                Console.WriteLine(client.ResponseHeaders);
+                client.DownloadFile(uri, path);
+                Console.Read();
+            }
+        }
+
+        private static void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            Console.WriteLine("Download completed!");
+        }
+
+    }
+}
